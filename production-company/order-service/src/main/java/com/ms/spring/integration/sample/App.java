7ca13cb58ff2d4408bb918a9fdcba9cf7ffe0ac3
Marcin Stachowiak <marcin.stachowiak.ms@gmail.com>
2017-09-30 12:12:17 +0200

    Added setup for projects

---
+++ production-company/order-service/src/main/java/com/ms/spring/integration/sample/App.java
@@ -0,0 +1,13 @@
+package com.ms.spring.integration.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
